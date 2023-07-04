ebf7d4ffba9dd4434ad29e5888b5a251a4517e0c
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-07-04 14:37:23 -0300

    instanciando variaveis globais e linkando elas ao seus respectivos ids

--- app/src/main/java/com/example/apresentacaoandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/apresentacaoandroidstudio/MainActivity.kt
@@ -6,12 +6,16 @@ import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    /**lateinit é utilizado para instanciar uma variavel global*/
+    /**lateinit é utilizado para Declarar uma variavel global*/
     lateinit var campo1: EditText
     lateinit var botao1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /** sempre escrever o codigo depois de abrir o layout */
+        /**Instanciando a variavel e atribuindo o metodo para pegar o id do componente
+         * e linkar ele com a variavel global */
+        campo1 = findViewById(R.id.nome)
+        botao1 = findViewById(R.id.botao)
     }
 }
