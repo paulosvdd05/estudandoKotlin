aded738e6b5e785dcbe9abe07f16eb789f77f7f3
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-07-04 14:33:43 -0300

    instanciando variaveis globais

--- app/src/main/java/com/example/apresentacaoandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/apresentacaoandroidstudio/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.apresentacaoandroidstudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    /**lateinit é utilizado para instanciar uma variavel global*/
+    lateinit var campo1: EditText
+    lateinit var botao1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /** sempre escrever o codigo depois de abrir o layout */
     }
 }
